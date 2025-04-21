c543e19e5e067d13e3d07999db97f028e61a8ddc
gururajchoudri55 <gurunath@amazeinc.in>
2025-04-21 15:46:25 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'Project',component:ProjectsComponent},
+    {path:'Home',component:HomeComponent},
+
+
+];
